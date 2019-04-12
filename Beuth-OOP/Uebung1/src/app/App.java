30ed7a63152f079bbbb5d896bcc81a4050ab63b8
currychris <christoph.gotschlich@gmail.com>
2019-04-12 12:33:28 +0200

    1.3

--- Beuth-OOP/Uebung1/src/app/App.java
+++ Beuth-OOP/Uebung1/src/app/App.java
@@ -1,7 +1,21 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        do{   
+            System.out.print("Bitte geben Sie eine ganze Zahl ein: ");
+            input = scanner.nextInt(); 
+        }while(evalInput(input) == false);
+
+        scanner.close();
+    }
+
+    private static boolean evalInput(int num){
+        if(num > 0) return true;
+        return false;
     }
 }
