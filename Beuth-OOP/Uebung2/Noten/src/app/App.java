ecc4e520d13dbea18e4f13845ea64da9d27048b7
currychris <christoph.gotschlich@gmail.com>
2019-04-15 16:10:45 +0200

    initial

--- Beuth-OOP/Uebung2/Noten/src/app/App.java
+++ Beuth-OOP/Uebung2/Noten/src/app/App.java
@@ -1,7 +1,31 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Note: ");
+        int note = in.nextInt();
+        String notenString;
+
+        switch(note){
+            case 1: notenString = "Sehr gut";
+                    break;
+            case 2: notenString = "Gut";
+                    break;
+            case 3: notenString = "Befriedigend";
+                    break;
+            case 4: notenString = "Ausreichend";
+                    break;
+            case 5: notenString = "Mangelhaft";
+                    break;
+            case 6: notenString = "Ungenügend";
+                    break;
+            default: notenString = "Fehlerhafte Eingabe";
+        }
+
+        System.out.println(notenString);
+
+        in.close();
     }
 }
